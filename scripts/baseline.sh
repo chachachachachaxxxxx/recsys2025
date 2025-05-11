04f659e8d7d6f0523026bb252723a15cbcbceabf
Xinxing Wang <205373694@qq.com>
2025-05-11 16:04:29 +0800

    feat: add baseline script for creating embeddings and update .gitignore

---
+++ scripts/baseline.sh
@@ -0,0 +1,5 @@
+python -m baseline.aggregated_features_baseline.create_embeddings \
+  --data-dir /home/xxx/data/ubc_data/ubc_data_submission \
+  --embeddings-dir /home/xxx/code/recsys2025/exp/embeddings1 \
+  --num-days 1 7 30 \
+  --top-n 10
